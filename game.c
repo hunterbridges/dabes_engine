60a208fbda6ee8b4557156a6215afc32673e2fc5
Hunter Bridges <hbridges@gmail.com>
2013-01-29 13:30:48 -0800

    OpenGL like a boss

--- game.c
+++ game.c
@@ -1,28 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "SDL/SDL.h"
+#include "SDL/SDL_opengl.h"
 #include "SDL_TTF.h"
 #include "dbg.h"
 #include "gameobjects.h"
 
 #define FPS 60
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define NUM_BOXES 10
 
 Object ThingProto = {
    .init = Thing_init,
    .calc_physics = Thing_calc_physics
 };
 
+int initGL() {
+    glEnable(GL_TEXTURE_2D);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f, 0.f);
+    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glShadeModel (GL_FLAT);
+    GLenum error = glGetError();
+    check(error == GL_NO_ERROR, "OpenGL init error...");
+    return 1;
+error:
+    printf("Error initializing OpenGL! %s\n", gluErrorString(error));
+    return 0;
+}
+
+int loadBgTexture(SDL_Surface *surface) {
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, surface->w, surface->h,
+        0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    //glBindTexture(GL_TEXTURE_2D, 0);
+    SDL_FreeSurface(surface);
+    return texture;
+}
+
 int Game_init(void *self) {
     check_mem(self);
 
     Game *game = (Game *)self;
     int i = 0;
-    for (i = 0; i < 200; i++) {
+    for (i = 0; i < NUM_BOXES; i++) {
         Thing *thing = NEW(Thing, "A thing");
-        thing->x = i * (640 / 200);
+        thing->width = SCREEN_WIDTH / (2 * NUM_BOXES);
+        thing->height = thing->width;
+        thing->x = i * (SCREEN_WIDTH / NUM_BOXES) + thing->width / 2;
         thing->y = i;
-        thing->width = 2;
-        thing->height = 2;
         thing->mass = 1000;
         thing->time_scale = (i + 1) / 400.0;
         game->things[i] = thing;
@@ -68,7 +109,8 @@ SDL_Surface *graydient(unsigned int width, unsigned int height) {
 
 int main(int argc, char *argv[])
 {
-    int quit = 0;
+    float swidth = SCREEN_WIDTH;
+    float sheight = SCREEN_HEIGHT;
 
     SDL_Event event = {};
     SDL_Surface *screen = NULL;
@@ -80,17 +122,27 @@ int main(int argc, char *argv[])
 
     Game *game = NEW(Game, "The game");
 
-    screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+    screen = SDL_SetVideoMode((int)swidth, (int)sheight, 32, SDL_OPENGLBLIT);
     Uint32 black = SDL_MapRGBA(screen->format, 0, 0, 0, 255);
 
+    check(initGL() == 1, "Init OpenGL");
+
     SDL_Surface *bg = graydient(640, 480);
+    int texture = loadBgTexture(bg);
 
     int skip = 1000 / FPS;
 
     long unsigned int last_frame_at = 0;
     short int init = 0;
-    SDL_BlitSurface(bg, NULL, screen, NULL);
 
+    float xadj = -1;
+    float yadj = 1;
+    float gProjectionScale = 1.0;
+    float gRotation = 0;
+    int reset = 0;
+    int quit = 0;
+    int zoom = 0;
+    int rot = 0;
     while (quit == 0) {
         long unsigned int ticks = SDL_GetTicks();
         long unsigned int ticks_since_last = ticks - last_frame_at;
@@ -108,31 +160,78 @@ int main(int argc, char *argv[])
 
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_KEYDOWN) {
-                if (event.key.keysym.sym == SDLK_q) {
-                    quit = 1;
-                }
+                if (event.key.keysym.sym == SDLK_q) quit = 1;
+                if (event.key.keysym.sym == SDLK_r) reset = 1;
+                if (event.key.keysym.sym == SDLK_j) zoom += 1;
+                if (event.key.keysym.sym == SDLK_k) zoom -= 1;
+                if (event.key.keysym.sym == SDLK_u) rot += 1;
+                if (event.key.keysym.sym == SDLK_i) rot -= 1;
+            }
+            if (event.type == SDL_KEYUP) {
+                if (event.key.keysym.sym == SDLK_j) zoom -= 1;
+                if (event.key.keysym.sym == SDLK_k) zoom += 1;
+                if (event.key.keysym.sym == SDLK_u) rot -= 1;
+                if (event.key.keysym.sym == SDLK_i) rot += 1;
             }
         }
 
         if (frame) {
-            int i = 0;
-            for (i = 0; i < 256; i++) {
-                Thing *thing = game->things[i];
-                if (thing == NULL) break;
-                SDL_Rect rect = thing->rect(thing);
-                SDL_BlitSurface(bg, &rect, screen, &rect);
+            if (reset) {
+                gProjectionScale = 1;
+                gRotation = 0;
+                reset = 0;
             }
+            gProjectionScale += 0.01 * zoom;
+            gRotation += 1 * rot;
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            glTranslatef(-SCREEN_WIDTH / 2.f,-SCREEN_HEIGHT / 2.f, 0.f );
+
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glOrtho(SCREEN_WIDTH / -2 * gProjectionScale,
+                    SCREEN_WIDTH / 2 * gProjectionScale,
+                    SCREEN_HEIGHT / 2 * gProjectionScale,
+                    SCREEN_HEIGHT / -2 * gProjectionScale,
+                    1.0, -1.0 );
+            glRotatef(gRotation, 0, 0, -1);
+            glClear(GL_COLOR_BUFFER_BIT);
+            int i = 0;
 
             SDL_Rect debugRect = {10, 10, 200, 100};
-            SDL_BlitSurface(bg, &debugRect, screen, &debugRect);
+            glBindTexture(GL_TEXTURE_2D, texture);
+            glBegin(GL_QUADS);
+                glColor3f(1.f, 1.f, 1.f);
+                glTexCoord2f(0, 0);
+                glVertex2f(0, 0);
+                glTexCoord2f(1, 0);
+                glVertex2f(SCREEN_WIDTH, 0);
+                glTexCoord2f(1, 1);
+                glVertex2f(SCREEN_WIDTH, SCREEN_HEIGHT);
+                glTexCoord2f(0, 1);
+                glVertex2f(0, SCREEN_HEIGHT);
+            glEnd();
+            glBindTexture(GL_TEXTURE_2D, 0);
+            //SDL_BlitSurface(bg, &debugRect, screen, &debugRect);
 
             game->_(calc_physics)(game, ticks_since_last);
 
-            for (i = 0; i < 256; i++) {
+            for (i = 0; i < NUM_BOXES; i++) {
                 Thing *thing = game->things[i];
                 if (thing == NULL) break;
                 SDL_Rect rect = thing->rect(thing);
-                SDL_FillRect(screen, &rect, black);
+                glBegin(GL_QUADS);
+                    glColor3f( 0.f, 0.f, 0.f );
+                    glVertex2f(rect.x,
+                               rect.y);
+                    glVertex2f((rect.x + rect.w),
+                               rect.y);
+                    glVertex2f((rect.x + rect.w),
+                               (rect.y + rect.h));
+                    glVertex2f((rect.x),
+                               (rect.y + rect.h));
+                glEnd();
+                //SDL_FillRect(screen, &rect, black);
             }
 
             SDL_Color txtBlack = {0,0,0,255};
@@ -145,15 +244,21 @@ int main(int argc, char *argv[])
             SDL_BlitSurface(debugText, NULL, screen, &debugRect);
             SDL_FreeSurface(debugText);
 
-            SDL_Flip(screen);
+            SDL_GL_SwapBuffers();
             last_frame_at = ticks;
         }
     }
 
+    glDeleteTextures(1, &texture);
     game->_(destroy)(game);
-    SDL_FreeSurface(bg);
+    //SDL_FreeSurface(bg);
     SDL_FreeSurface(screen);
     TTF_CloseFont(font);
 
+    return 0;
+error:
+    game->_(destroy)(game);
+    SDL_FreeSurface(screen);
+    TTF_CloseFont(font);
     return 1;
 }
