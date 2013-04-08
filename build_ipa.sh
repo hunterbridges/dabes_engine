a8525e44ba6a674e7a0807eec1baa5e512782996
Hunter Bridges <hbridges@gmail.com>
2013-04-08 10:48:57 -0700

    IPA build script

---
+++ build_ipa.sh
@@ -0,0 +1,9 @@
+PARAM_SIGNING_IDENTITY="iPhone Distribution: Meedeor, LLC"
+cd "platform/ios/DaBesEngine"
+WORKSPACE=`pwd`
+xcodebuild -target "DaBesEngine" -configuration "Release" BUILD_RELEASE_DEPENDENCIES=0 ARCHIVE_AFTER_BUILD=0 CODE_SIGN_IDENTITY="$PARAM_SIGNING_IDENTITY" CODE_SIGN_REQUIRED=YES clean build
+/usr/bin/xcrun -sdk iphoneos PackageApplication \
+    -v "$WORKSPACE/build/Release-iphoneos/DaBesEngine.app" \
+    -o "$WORKSPACE/build/DaBesEngine.ipa" \
+    --sign "${PARAM_SIGNING_IDENTITY}" \
+    --embed "/Users/hbridges/Library/MobileDevice/Provisioning Profiles/47AEF975-F53F-4F5C-95BC-DB23991E3E90.mobileprovision"
