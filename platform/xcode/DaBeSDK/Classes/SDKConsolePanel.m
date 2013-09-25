e2c9fe6e1400aed143acfdc0aaa713083bafb82a
Levi Kennedy <lckennedy@gmail.com>
2013-09-24 22:46:08 -0700

    Console for script editor

--- platform/xcode/DaBeSDK/Classes/SDKConsolePanel.m
+++ platform/xcode/DaBeSDK/Classes/SDKConsolePanel.m
@@ -8,6 +8,35 @@
 
 #import "SDKConsolePanel.h"
 
+@interface SDKConsolePanel ()
+@property (nonatomic, strong) NSMutableArray *messages;
+@end
+
 @implementation SDKConsolePanel
+
+- (void)log:(NSString *)message withLevel:(NSString *)level {
+  [self.messages addObject:[message copy]];
+  
+  NSColor *color;
+  NSFont *font = [NSFont fontWithName:@"Menlo" size:14.0];
+
+  if ([level isEqualToString:@"WARN"]) {
+    color = [NSColor orangeColor];
+  } else if ([level isEqualToString:@"ERROR"]) {
+    color = [NSColor redColor];
+  } else {
+    color = [NSColor blackColor];
+  }
+  
+  NSDictionary *attrs = @{
+                          NSForegroundColorAttributeName: color,
+                          NSFontAttributeName: font
+  };
+  
+  NSAttributedString *coloredMessage = [[NSAttributedString alloc] initWithString:[message copy] attributes: attrs];
+  
+  [self.consoleView insertText:coloredMessage];
+  [self.consoleView insertText:@"\n"];
+}
 
 @end
